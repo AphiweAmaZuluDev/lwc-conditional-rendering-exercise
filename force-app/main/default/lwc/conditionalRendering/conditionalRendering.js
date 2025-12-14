442f59615081d8d770f8087005a085dee43fbefb
Aphiwe Zulu <azulu058@gmail.com>
2025-12-14 19:28:38 +0200

    Restructured Div blocks

--- force-app/main/default/lwc/conditionalRendering/conditionalRendering.js
+++ force-app/main/default/lwc/conditionalRendering/conditionalRendering.js
@@ -1,15 +1,24 @@
 import { LightningElement, track } from 'lwc';
 
 export default class ConditionalRendering extends LightningElement {
+    // default component variables
+    @track likeState = false;
+    @track answerState = false;
+    @track likeStateSize01 = false;
+    @track likeStateSize02 = false;
+    @track likeStateSize03 = false;
+    @track likeStateSize04 = false;
+    @track likeStateDisabled = false;
+    @track answerStateDisabled = false;
 
-    // Variables
 
+    // Custom variables to practice conditional rendering
     displaySetting = true
     displaySimple = false
     displaySizes = false
     displayDisabled = false
 
-    // Methods
+    // Custom methods to practice conditional rendering
 
     toggleDisplaySimple(event) {
         this.displaySimple = this.displaySimple === false ? true : false
@@ -27,5 +36,31 @@ export default class ConditionalRendering extends LightningElement {
         this.displayDisabled = this.displayDisabled === false ? true : false
         this.displaySizes = false
         this.displaySimple = false
+    }
+
+    // ----------------------------
+
+    // default component methods
+    handleLikeButtonClick() {
+        this.likeState = !this.likeState;
+    }
+
+    handleAnswerButtonClick() {
+        this.answerState = !this.answerState;
+    }
+
+    handleLikeButtonSizeClick(event) {
+        const buttonNumber = event.target.dataset.buttonNumber;
+
+        this[`likeStateSize${buttonNumber}`] =
+            !this[`likeStateSize${buttonNumber}`];
+    }
+
+    handleLikeButtonDisabledClick() {
+        this.likeStateDisabled = !this.likeStateDisabled;
+    }
+
+    handleAnswerButtonDisabledClick() {
+        this.answerStateDisabled = !this.answerStateDisabled;
     }
 }
